c25ce176f0b913cd82b2042214d1fa76329a8acb
Jenny Zhang <jenzhang530@gmail.com>
2024-03-31 18:32:23 -0400

    Remove displayName

---
+++ prisma/migrations/20240331223205_remove_display_name/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `displayImage` on the `Shop` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Shop" DROP COLUMN "displayImage";
