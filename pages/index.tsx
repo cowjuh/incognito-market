d8ff6eacac42f6e25d425d06c9e2e2fb084ad4f9
Jenny Zhang <jenzhang530@gmail.com>
2024-03-31 16:19:08 -0400

    test: Upload image and add its URL to appropriate shop object

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,55 @@
-import React from "react"
+import React, { useRef } from "react"
 import Layout from "../components/Layout"
 import { PostProps } from "../components/Post"
-import { createClient } from "@supabase/supabase-js";
+import supabase, { SupabaseStorageUploadResponse } from '../utils/supabaseClient';
 import Image from "next/image";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 type Props = {
   feed: PostProps[]
 }
 
 const Blog: React.FC<Props> = () => {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
-
+  const fileInput = useRef(null);
   const data = supabase.storage.from('media').getPublicUrl('users/6D167EFA-237A-4BF4-B4CE-D15BFA66B189.JPG')
 
-  React.useEffect(() => {
-    console.log('data', data);
-  }, [data]);
+  const handleUpload = async () => {
+    const file = fileInput.current.files[0];
+    const filePath = `users/${file.name}`;
+
+    if (file) {
+      let { error, data } = await supabase.storage.from('media').upload(filePath, file);
+      if (error) {
+        console.error('Error uploading file: ', error);
+      } else {
+        const uploadData = data as SupabaseStorageUploadResponse;
+        console.log('File uploaded successfully', uploadData);
+        // Construct the public URL of the uploaded file
+        const publicURL = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/${uploadData.fullPath}`;
+
+        // Update the Shop record with the new image path
+        const response = await fetch('/api/shop', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: 'clufxw74e00014fxsthgnbfd7',
+            publicUrl: publicURL,
+          }),
+        });
+
+        if (!response.ok) {
+          console.error('Error updating Shop: ', await response.text());
+        } else {
+          const responseData = await response.json();
+          console.log('Response data: ', responseData);
+        }
+      }
+    }
+  };
 
 
   return (
@@ -27,6 +58,8 @@ const Blog: React.FC<Props> = () => {
         <h1>Public Feed</h1>
         <main>
           <Image src={data.data.publicUrl} alt="Picture of the author" width={500} height={500} className="w-[200px] h-auto rounded-sm" />
+          <input type="file" ref={fileInput} />
+          <button onClick={handleUpload}>Upload image</button>
         </main>
       </div>
     </Layout>
