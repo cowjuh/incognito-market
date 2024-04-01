ee76f2eaf595f2c31d16a7ae1ed44a2e50a059ed
Jenny Zhang <jenzhang530@gmail.com>
2024-03-31 21:55:35 -0400

    Add main content panel to slug

--- pages/shop/[slug].tsx
+++ pages/shop/[slug].tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import { useShop } from "../../hooks/useShop";
-import Image from "next/image";
 import MainLayout from "../../layout/MainLayout";
 import { ReactNode } from "react";
 import { NextPageWithLayout } from "../../layout/NextPageWithLayout";
 import PaddedLayout from "../../layout/PaddedLayout";
 import Link from "next/link";
+import Avatar from "@/Avatar";
 
 const ShopPage: NextPageWithLayout = () => {
     const router = useRouter();
@@ -13,19 +13,21 @@ const ShopPage: NextPageWithLayout = () => {
     const { shop } = useShop(slug as string);
 
     return (
-        <div>
-            <Link href={`/`}>
-                Back
-            </Link>
-            <h1>Shop: {slug}</h1>
-            {shop &&
-                <div>
-                    <h2>{shop.name}</h2>
-                    <p>{shop.bio}</p>
-                    <p>{shop.email}</p>
-                    <Image src={shop.profilePicture} alt="Picture of the author" width={500} height={500} className="w-20 h-auto" />
-                </div>
-            }
+        <div className="flex w-full h-full divide-x">
+            <div className="w-[20%] max-w-[400px] flex flex-col gap-4 flex-grow">
+                <Link href={`/`}>
+                    Back
+                </Link>
+                {shop &&
+                    <div className="flex flex-col gap-4">
+                        <Avatar src={shop.profilePicture} alt="Picture of the author" className="h-20 w-20" />
+                        <h2 className="text-lg">{shop.name}</h2>
+                        <p>{shop.bio}</p>
+                        <p>{shop.email}</p>
+                    </div>
+                }
+            </div>
+            <div className="p-4">main content</div>
         </div>
     );
 }
