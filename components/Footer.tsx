b7100c57ae6c717f57848896bf664469649910bd
Jenny Zhang <jenzhang530@gmail.com>
2024-03-31 20:41:01 -0400

    Create Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='flex items-center w-full justify-between px-4 py-2 border-t'>
+            2024 • developed by Jenny Zhang
+        </footer>
+    );
+}
+
+export default Footer;
