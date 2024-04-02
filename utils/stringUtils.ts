9f2f07dcccf78f1e0229fc710c6c6210912a198b
Jenny Zhang <jenzhang530@gmail.com>
2024-04-01 23:15:19 -0400

    Add IG and twitter URL parsing functions

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,3 +1,15 @@
 export function formatPlural(count: number, singular: string, plural: string) {
     return `${count} ${count === 1 ? singular : plural}`;
 }
+
+export function getUsernameFromInstagramUrl(url: string): string {
+    const urlParts = url.split('/');
+    const usernameIndex = urlParts.indexOf('instagram.com') + 1;
+    return urlParts[usernameIndex];
+}
+
+export function getUsernameFromTwitterUrl(url: string): string {
+    const urlParts = url.split('/');
+    const usernameIndex = urlParts.indexOf('twitter.com') + 1;
+    return urlParts[usernameIndex];
+}
