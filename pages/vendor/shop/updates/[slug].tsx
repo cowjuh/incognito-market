b7261b4f38efa3588fa4adb534b9da6c8995270f
Jenny Zhang <jenzhang530@gmail.com>
2024-04-21 12:19:54 -0400

    Use components and repurposed dialog

--- pages/vendor/shop/updates/[slug].tsx
+++ pages/vendor/shop/updates/[slug].tsx
@@ -1,29 +1,36 @@
+import UpdateCard from "@/shop/UpdateCard";
 import { Button } from "@/ui/button";
 import CreateUpdateDialog from "@/vendor/updates/CreateUpdateDialog";
+import { Pencil1Icon } from "@radix-ui/react-icons";
 import { useShop } from "hooks/useShop";
+import { useShopUpdates } from "hooks/useShopUpdates";
 import FullWidthLayout from "layout/FullWidthLayout";
 import MainLayout from "layout/MainLayout";
 import { NextPageWithLayout } from "layout/NextPageWithLayout";
 import VendorShopLayout from "layout/VendorShopLayout";
 import { useRouter } from "next/router";
+import { FormMode } from "types/form";
 import { formatPostedAt } from "utils/stringUtils";
 
 const ShopUpdatesPage: NextPageWithLayout = () => {
     const router = useRouter();
     const { slug } = router.query;
-    const { shop } = useShop(slug as string);
+    const { updates } = useShopUpdates({ shopId: slug as string });
     return <div>
         <div className="flex flex-col gap-4">
-            <div className="flex justify-between items-center">
-                <h1 className="text-xl">Updates</h1>
-                <CreateUpdateDialog shopId={shop?.id} />
-            </div>
-            {shop?.updates.map((update) => (
-                <div key={update.id} className="flex flex-col gap-2 p-4 border rounded-md">
-                    <div className="font-medium">{update.title}</div>
-                    <div>{update.content}</div>
-                    <div className="text-neutral-500">{formatPostedAt(String(update.postedAt))}</div>
+            <div className="flex justify-between items-start">
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-xl">Announcements</h1>
+                    <p className="text-sm text-muted-foreground">Make and modify announcements</p>
                 </div>
+                <CreateUpdateDialog shopId={slug as string} mode={FormMode.CREATE}>
+                    <Button>
+                        Create Update
+                    </Button>
+                </CreateUpdateDialog>
+            </div>
+            {updates.map((update) => (
+                <UpdateCard key={update.id} update={update} shopId={slug as string} isVendorView />
             ))}
         </div>
     </div>;
