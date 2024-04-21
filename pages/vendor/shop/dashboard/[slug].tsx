cb2b1e1029646cf4ebe71b9b4479fff267af9e12
Jenny Zhang <jenzhang530@gmail.com>
2024-04-20 21:28:59 -0400

    Update slug for shop dashboard

---
+++ pages/vendor/shop/dashboard/[slug].tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/ui/button";
+import ShopSettingsSidebar from "@/vendor/manage/ShopSettingsSidebar";
+import { useShop } from "hooks/useShop";
+import FullWidthLayout from "layout/FullWidthLayout";
+import MainLayout from "layout/MainLayout";
+import { NextPageWithLayout } from "layout/NextPageWithLayout";
+import PaddedLayout from "layout/PaddedLayout";
+import VendorShopLayout from "layout/VendorShopLayout";
+import { useRouter } from "next/router";
+
+const VendorShopPage: NextPageWithLayout = () => {
+    const router = useRouter();
+    const { slug } = router.query;
+    const { shop, loading, error } = useShop(slug as string);
+    const showSkeleton = !shop || loading;
+
+    const handleVisit = () => {
+        router.push(`/shop/${slug}`);
+    }
+
+    if (!shop) return null;
+
+    return (
+        <div className="w-full">
+            <div className="w-full flex items-center justify-between p-4">
+                <h1 className="text-xl">{shop.name}</h1>
+                <div className="flex items-center gap-4">
+                    <p className="text-sm text-neutral-500">Published 20 mins ago</p>
+                    <Button onClick={handleVisit}>
+                        Visit
+                    </Button>
+                </div>
+            </div>
+            <p>{shop.description}</p>
+        </div>
+    )
+}
+
+VendorShopPage.getLayout = (page) => {
+    return (
+        <MainLayout>
+            <FullWidthLayout className="flex flex-row flex-grow py-0">
+                <VendorShopLayout>
+                    {page}
+                </VendorShopLayout>
+            </FullWidthLayout>
+        </MainLayout>
+    )
+};
+
+export default VendorShopPage;
+
