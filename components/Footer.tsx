88b50b63d330e26b9937f993cbe3d0230116f44a
Jenny Zhang <jenzhang530@gmail.com>
2024-04-20 21:26:25 -0400

    Add email to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className='flex items-center w-full justify-between px-4 py-2 border-t h-10 text-neutral-400'>
-            2024 • developed by Jenny Zhang
+            2024 • developed by Jenny Zhang • hello@4o4.space
         </footer>
     );
 }
