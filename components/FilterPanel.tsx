82e627d3a2eb931686aee8c08979779041b01fe3
Jenny Zhang <jenzhang530@gmail.com>
2024-06-27 12:26:21 -0400

    Highlight selected category

--- components/FilterPanel.tsx
+++ components/FilterPanel.tsx
@@ -2,10 +2,17 @@ import { Tag } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getAllTags } from "services/api/tags";
+import { cn } from "./lib/utils";
+
+const tagNameToSlug = (tagName: string) => {
+    return tagName.toLowerCase().replace(/\s+/g, '-');
+}
 
 const FilterPanel = () => {
     const [allTags, setAllTags] = useState<Tag[]>([]);
     const router = useRouter();
+    const isTagPage = router.pathname.startsWith('/tag');
+    const slug = router.query.slug as string;
 
     useEffect(() => {
         getAllTags().then((tags) => {
@@ -19,11 +26,24 @@ const FilterPanel = () => {
     }
 
     return (
-        <div className="w-[20%] max-w-[400px] min-w-[250px] p-4">
+        <div className="w-[20%] max-w-[400px] min-w-[250px] p-4 flex flex-col gap-2">
             <h3 className="font-medium text-neutral-400">CATEGORIES</h3>
-            {allTags.map((tag) => (
-                <div key={tag.id} onClick={() => handleClickTag(tag)}>{tag.name}</div>
-            ))}
+            {allTags.map((tag) => {
+                const isHighlighted = isTagPage && tagNameToSlug(tag.name) === slug;
+                return (
+                    <div
+                        key={tag.id}
+                        onClick={() => handleClickTag(tag)}
+                        className={cn(
+                            'cursor-pointer hover:font-semibold py-1 px-2 rounded-md',
+                            isHighlighted ? 'bg-neutral-200 font-semibold' : ''
+                        )}
+                    >{tag.name}</div>
+
+                )
+            }
+
+            )}
         </div>
     )
 }
