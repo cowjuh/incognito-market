9b5bd4c8be786e6eccd027287e4e962a6805f0c9
Jenny Zhang <jenzhang530@gmail.com>
2024-11-10 12:24:47 -0500

    Install react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,27 @@ import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import "../styles/globals.css";
 import { NextPageWithLayout } from '../layout/NextPageWithLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <SessionProvider session={pageProps.session}>
-      {getLayout(
-        <Component {...pageProps} />
-      )}
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        {getLayout(
+          <Component {...pageProps} />
+        )}
+      </SessionProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
